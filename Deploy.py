cd01e2064d2146d632a6d9a7ed9ec1567892d9e6
pmecchia <mecchia.pierre@gmail.com>
2021-11-28 14:29:56 +0100

    udpate print error

--- Deploy.py
+++ Deploy.py
@@ -1,11 +1,11 @@
 import subprocess
 
-file = open('list_ip.txt', 'r')
+file = open('INF727_Systemes_repartis/list_ip.txt', 'r')
 ip_list = file.read().splitlines()
 
 def ssh(command,machines):
     listproc = []
-    timer=5
+    #timer=5
     login="pmecchia-20"
 
     for ip in machines:
@@ -14,11 +14,12 @@ def ssh(command,machines):
 
     for i in range(len(listproc)):
         try:
-            out, err = listproc[i].communicate(timeout=timer)
+            out, err = listproc[i].communicate()
             code = listproc[i].returncode
-            print(str(i)+" out: '{}'".format(out))
-            print(str(i)+" err: '{}'".format(err))
-            print(str(i)+" exit: {}".format(code))
+            if code!=0:
+                print(str(i)+" out: '{}'".format(out))
+                print(str(i)+" err: '{}'".format(err))
+                print(str(i)+" exit: {}".format(code))
         except subprocess.TimeoutExpired:
             listproc[i].kill()
             print(str(i)+" timeout")
@@ -28,7 +29,7 @@ def ssh(command,machines):
 
 def scp(localPath,distantPath,machines):
     listproc = []
-    timer=5
+    #timer=200
     login="pmecchia-20"
     for ip in machines:
         proc = subprocess.Popen(["scp",localPath,login+"@"+ip+":"+distantPath],stdin=subprocess.PIPE, stdout = subprocess.PIPE,stderr = subprocess.PIPE)
@@ -36,17 +37,18 @@ def scp(localPath,distantPath,machines):
 
     for i in range(len(listproc)):
         try:
-            out, err = listproc[i].communicate(timeout=timer)
+            out, err = listproc[i].communicate()
             code = listproc[i].returncode
-            print(str(i)+" out: '{}'".format(out))
-            print(str(i)+" err: '{}'".format(err))
-            print(str(i)+" exit: {}".format(code))
+            if code !=0:
+                print(str(i)+" out: '{}'".format(out))
+                print(str(i)+" err: '{}'".format(err))
+                print(str(i)+" exit: {}".format(code))
         except subprocess.TimeoutExpired:
             listproc[i].kill()
             print(str(i)+" timeout")
 
 ip_list=ssh("mkdir -p /tmp/pmecchia-20",ip_list)
-scp("./Slave.py", "/tmp/pmecchia-20",ip_list)
+scp("INF727_Systemes_repartis/Slave.py", "/tmp/pmecchia-20",ip_list)
 ssh("ls /tmp/pmecchia-20",ip_list)
 
 
